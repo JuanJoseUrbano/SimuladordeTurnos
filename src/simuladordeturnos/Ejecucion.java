c9a407fc59e0ae4ae113726951fef41478afb1cc
JuanJoseUrbano <urbanoperdomoj>
2023-05-04 15:37:45 -0500

    Este es una madificacio para la subrama

--- src/simuladordeturnos/Ejecucion.java
+++ src/simuladordeturnos/Ejecucion.java
@@ -4,7 +4,7 @@ package simuladordeturnos;
 public class Ejecucion {
 
     public static void main(String[] args) {
-        System.out.println("Este es un cambio");
+        System.out.println("Este es un cambioEste es ");
         SolicitarTurno st = new SolicitarTurno();
         st.setVisible(true);
         st.setTitle("Registrarse");
