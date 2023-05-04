832d98f7e939b1f5fce07c577f889025acce839d
JuanJoseUrbano <urbanoperdomoj>
2023-05-04 15:34:54 -0500

    este es un nuevo cambio

--- src/simuladordeturnos/Ejecucion.java
+++ src/simuladordeturnos/Ejecucion.java
@@ -4,7 +4,7 @@ package simuladordeturnos;
 public class Ejecucion {
 
     public static void main(String[] args) {
-        
+        System.out.println("Este es un cambio");
         SolicitarTurno st = new SolicitarTurno();
         st.setVisible(true);
         st.setTitle("Registrarse");
